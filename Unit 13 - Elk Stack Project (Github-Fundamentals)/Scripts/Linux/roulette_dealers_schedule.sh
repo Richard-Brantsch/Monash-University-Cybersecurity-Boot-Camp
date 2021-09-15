f0ce185f6fe28ebe9d4a9fbeaecb395a1b997952
Richard Brantsch <richard.brantsch.88@gmail.com>
2021-09-15 17:16:12 +1000

    changed index

---
+++ Unit 13 - Elk Stack Project (Github-Fundamentals)/Scripts/Linux/roulette_dealers_schedule.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#usage .roulette_dealers_schedule.sh (date) (time) (AM/PM)
+cat $1_Dealer_schedule | awk -F"\t" '{print $1, $3}' | grep $2 | grep $3 >> Dealers_working_during_losses
+
